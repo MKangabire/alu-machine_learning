f1451395dfbff7a53b73a4dc1b891e4b80f4f581
MKangabire <m.kangabire@alustudent.com>
2024-09-30 09:08:23 +0000

    calc

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -6,8 +6,4 @@ def summation_i_squared(n):
     """a function that calculates the sum of 1 to n"""
     if not isinstance(n, int):
         return None
-    solution = 0
-    for i in range(1,n+1):
-        b = i**2
-        solution += b
-    return solution
+    return n*(n+1)*(2*n+1)//6      
