7c17e9745497e62d845f23053f84e910d65ab4b6
MKangabire <m.kangabire@alustudent.com>
2024-10-23 12:38:48 +0200

    convolution

---
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -0,0 +1,18 @@
+#!/usr/bin python3
+"""a function that performs a valid convolution on grayscale img"""
+import numpy as np
+
+
+def convolve_grayscale_valid(images, kernel):
+    """a function that performs a valid convolution on grayscale img"""
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+    ph = h - kh + 1
+    pw = w - kw + 1
+    images_conv = np.zeros((m, ph, pw))
+    for i in range(m):
+      for j in range(h):
+        for k in range(w):
+          region = images[i, j:j+kh, k:k+kw]
+          images_conv[i, j, k] = np.sum(region * kernel)
+    return images_conv
