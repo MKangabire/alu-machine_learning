6ef382fb65c3cdd38bcb76137e96f9527e2ef1ef
MKangabire <m.kangabire@alustudent.com>
2025-01-17 09:06:32 +0200

    alu-machinelearning

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -1,7 +1,17 @@
+#!/usr/bin/env python3
+"""a neuron performing binary classification"""
+
+
 import numpy as np
 
 class Neuron:
+    """class neuron"""
+
+
     def __init__(self, nx):
+        """initializing method"""
+
+        
         self.nx = nx
         if not isinstance(nx, int):
             raise TypeError("nx must be a integer")
