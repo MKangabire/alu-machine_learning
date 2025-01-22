b33d4bc4058c4406c6c474a7f5c6ec4d63462414
MKangabire <m.kangabire@alustudent.com>
2025-01-22 09:54:19 +0200

    classification

--- supervised_learning/classification/6-neuron.py
+++ supervised_learning/classification/6-neuron.py
@@ -70,13 +70,13 @@ class Neuron:
             raise TypeError("iterations must be an integer")
 
         if iterations < 1:
-            raise ValueError("iterations must be positive integer")
+            raise ValueError("iterations must be a positive integer")
 
         if not isinstance(alpha, float):
             raise TypeError("alpha must be a float")
 
         if alpha <= 0:
-            raise ValueError('alpha must be a positive')
+            raise ValueError('alpha must be a positive integer')
 
         for _ in range(iterations):
             A = self.forward_prop(X)
