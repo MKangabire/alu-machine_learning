b82b8250db500549bd506e31d79fd1a509e2b826
MKangabire <m.kangabire@alustudent.com>
2025-03-06 08:46:10 +0000

    database

--- pipeline/databases/0-create_database_if_missing.sql
+++ pipeline/databases/0-create_database_if_missing.sql
@@ -2,13 +2,3 @@
 
 --creation of the database
 CREATE DATABASE IF NOT EXISTS db_0;
-DELIMETER //
-CREATE PROCEDURE create_db_0_if_not_exists()
-BEGIN
-	DECLARE continue_handler INT DEFAULT 0;
-	CREATE DATABASE db_0;
-	DECLARE CONTINUE HANDLER FOR SQLSTATE '42000'
-	     SET continue_handler = 1;
-	CREATE DATABASE db_0;
-END //
-CALL create_db_0_if_not_exists();
